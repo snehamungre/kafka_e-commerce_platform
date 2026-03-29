1fbd940a92e2959e8dea3cf586f3527c94f9937d
Sneha Mungre <snehamungre@gmail.com>
2026-03-29 17:49:41 +0530

    producer code

---
+++ orders.py
@@ -0,0 +1,23 @@
+import json
+import uuid
+from confluent_kafka import Producer
+
+producer_config = {"bootstrap.servers": "kafka:9092"}
+
+producer = Producer(producer_config)
+
+
+def delivery_report(err, msg):
+    if err:
+        print(f"Delivery failed: {err}")
+    else:
+        print(f"Delivered {msg.value().decode("utf-8")}")
+
+
+order = {"order_id": str(uuid.uuid4()), "user": "name", "item": "book", "quantity": 2}
+
+value = json.dumps(order).encode("utf-8")
+
+producer.produce(topic="order", value=value, callback=delivery_report)
+
+producer.flush()
