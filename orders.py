0ac717c2ee418d51bcafad02cf464ae2fda34952
Sneha Mungre <snehamungre@gmail.com>
2026-03-31 11:56:38 +0530

    required consumer and producers

--- orders.py
+++ orders.py
@@ -2,7 +2,7 @@ import json
 import uuid
 from confluent_kafka import Producer
 
-producer_config = {"bootstrap.servers": "kafka:9092"}
+producer_config = {"bootstrap.servers": "localhost:9092"}
 
 producer = Producer(producer_config)
 
@@ -12,9 +12,10 @@ def delivery_report(err, msg):
         print(f"Delivery failed: {err}")
     else:
         print(f"Delivered {msg.value().decode("utf-8")}")
+        print(f"Delivered to: {msg.topic()} and partition {msg.partition()}")
 
 
-order = {"order_id": str(uuid.uuid4()), "user": "name", "item": "book", "quantity": 2}
+order = {"order_id": str(uuid.uuid4()), "user": "man", "item": "pen", "quantity": 1}
 
 value = json.dumps(order).encode("utf-8")
 
